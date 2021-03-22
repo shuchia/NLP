b1b24d01df32727569f3358bc2bbb658ff2206b1
Shuchi Agarwal <shuchia@fipointer.com>
2021-03-22 14:30:48 -0400

    Fix function signatures

--- backend/app/inference.py
+++ backend/app/inference.py
@@ -35,16 +35,16 @@ class NLP:
         self.model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
         self.tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
 
-    def generate_summary(self, model, nested_sentences):
+    def generate_summary(self, nested_sentences):
         logger.info("Inside inference generate summary")
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         summaries = []
         for nested in nested_sentences:
-            input_tokenized = self.encode(' '.join(nested), truncation=True, return_tensors='pt')
+            input_tokenized = self.tokenizer.encode(' '.join(nested), truncation=True, return_tensors='pt')
             input_tokenized = input_tokenized.to(device)
-            summary_ids = model.to(device).generate(input_tokenized,
-                                                    length_penalty=3.0)
-            output = [self.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in
+            summary_ids = self.model.to(device).generate(input_tokenized,
+                                                         length_penalty=3.0)
+            output = [self.tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in
                       summary_ids]
             summaries.append(output)
         summaries = [sentence for sublist in summaries for sentence in sublist]
@@ -54,9 +54,9 @@ class NLP:
         logger.info("Inside inference after model load")
         nested = nest_sentences(article_text)
 
-        summarized_text = self.generate_summary(self.tokenizer, self.model, nested)
+        summarized_text = self.generate_summary(nested)
         nested_summ = nest_sentences(' '.join(summarized_text))
-        return self.generate_summary(self.tokenizer, self.model, nested_summ)
+        return self.generate_summary(nested_summ)
     # return nested
 
 # def inference(model_name, article_text):
