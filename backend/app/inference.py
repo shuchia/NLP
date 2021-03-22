e8a02fe841098fbb06bddda41ec01d1a78a0c89d
Shuchi Agarwal <shuchia@fipointer.com>
2021-03-22 09:51:24 -0400

    Refactor

--- backend/app/inference.py
+++ backend/app/inference.py
@@ -30,34 +30,33 @@ def nest_sentences(document):
     return nested
 
 
-def generate_summary(tokenizer, model, nested_sentences):
-    logger.info("Inside inference generate summary")
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    summaries = []
-    for nested in nested_sentences:
-        input_tokenized = tokenizer.encode(' '.join(nested), truncation=True, return_tensors='pt')
-        input_tokenized = input_tokenized.to(device)
-        summary_ids = model.to(device).generate(input_tokenized,
-                                                length_penalty=3.0)
-        output = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in
-                  summary_ids]
-        summaries.append(output)
-    summaries = [sentence for sublist in summaries for sentence in sublist]
-    return summaries
-
-
 class NLP:
     def __init__(self):
         self.model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
         self.tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
 
+    def generate_summary(self, model, nested_sentences):
+        logger.info("Inside inference generate summary")
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        summaries = []
+        for nested in nested_sentences:
+            input_tokenized = self.encode(' '.join(nested), truncation=True, return_tensors='pt')
+            input_tokenized = input_tokenized.to(device)
+            summary_ids = model.to(device).generate(input_tokenized,
+                                                    length_penalty=3.0)
+            output = [self.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in
+                      summary_ids]
+            summaries.append(output)
+        summaries = [sentence for sublist in summaries for sentence in sublist]
+        return summaries
+
     def inference(self, model_name, article_text):
         logger.info("Inside inference after model load")
         nested = nest_sentences(article_text)
 
-        summarized_text = generate_summary(self.tokenizer, self.model, nested)
+        summarized_text = self.generate_summary(self.tokenizer, self.model, nested)
         nested_summ = nest_sentences(' '.join(summarized_text))
-        return generate_summary(self.tokenizer, self.model, nested_summ)
+        return self.generate_summary(self.tokenizer, self.model, nested_summ)
     # return nested
 
 # def inference(model_name, article_text):
